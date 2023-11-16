6f52f871aa85df096734c5e127fc23f63e8acfa1
Pregum <pregum.y@gmail.com>
2023-11-17 07:44:25 +0900

    update: bottom navigation barを追加

--- lib/sandbox/dash_board_page.dart
+++ lib/sandbox/dash_board_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const RootPage());
@@ -166,14 +165,21 @@ class DashBoardPage extends HookWidget {
                   ),
                 ),
               ),
-              Flexible(
-                  flex: 2,
-                  child: Container(
-                      color: Colors.grey, margin: const EdgeInsets.all(8))),
+              // Flexible(
+              //     flex: 2,
+              //     child: Container(
+              //         color: Colors.grey, margin: const EdgeInsets.all(8))),
             ],
           ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'grand menu'),
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: '店舗一覧'),
+      ],),
+      // bottomNavigationBar: const PersistentBottomNavBar(
+      //   navBarStyle: NavBarStyle.style9,
+      // ),
     );
   }
 }
